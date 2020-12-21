47fd3e005300de86f6e6732956a60d844e2dac86
ambrwlsn <arw89@uclive.ac.nz>
2020-12-21 22:36:51 +0100

    Create dog app

---
+++ src/shuffle.js
@@ -0,0 +1,11 @@
+// https://stackoverflow.com/a/46161940/7087336
+const shuffle = (arr) => {
+  const newArr = arr.slice();
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+  }
+  return newArr;
+};
+
+export default shuffle;
